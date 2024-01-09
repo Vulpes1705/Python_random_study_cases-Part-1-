9e666ec193841adf6169c764c02f58360af4ca73
Alexander <109859387+Vulpes1705@users.noreply.github.com>
2024-01-09 23:07:34 +0300

    Another task complete
    Added second task from study case https://www.w3resource.com/python-exercises/python-basic-exercises.php (Basic level so far)

---
+++ 01_pyhton_study_case(what_version).py
@@ -0,0 +1,11 @@
+# Study beginner tasks 
+# from https://www.w3resource.com/python-exercises/python-basic-exercises.php 
+
+# THE TASK 
+#  Write a Python program to find out
+#  what version of Python you are using. 
+
+import sys #import sys module 
+
+my_python_version = sys.version
+print("My python version is " + str(my_python_version))
