2bd15d3597f41348ddc8bfa8437dc263d83f6e24
Alexander <109859387+Vulpes1705@users.noreply.github.com>
2024-01-11 15:01:46 +0300

    Solved another task
    Another task complete

---
+++ 02_pyhton_study_case(show_date_and_time).py
@@ -0,0 +1,18 @@
+# Study beginner tasks 
+# from https://www.w3resource.com/python-exercises/python-basic-exercises.php 
+
+# THE TASK 
+#  Write a Python program to display the current date and time.
+# Sample Output :
+# Current date and time :
+# 2014-07-05 14:34:14
+
+from datetime import datetime
+
+# # datetime object containing current date and time
+now = datetime.now()
+
+
+# dd/mm/YY H:M:S
+dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+print("Current date and time is", dt_string)
