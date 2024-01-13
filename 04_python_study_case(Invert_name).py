dece82affefb485e428c44257417720a9bb02fef
Alexander <109859387+Vulpes1705@users.noreply.github.com>
2024-01-13 22:13:38 +0300

    Add files via upload

---
+++ 04_python_study_case(Invert_name).py
@@ -0,0 +1,13 @@
+# Study beginner tasks 
+# from https://www.w3resource.com/python-exercises/python-basic-exercises.php 
+
+# THE TASK 
+#  Write a Python program that accepts the user's first and last name and prints them in reverse order with a space between them. 
+#  Уточнил, имеется в виду Иван Иванов -> Иванов Иван 
+
+first_name = input("Please enter your first name. ")
+second_name = input("Please enter your second name. ")
+
+print("Hello, " + second_name + " " + first_name + "!")
+
+
