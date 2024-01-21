37a918975c668a76d11798b31c968faf86399f58
Alexander <109859387+Vulpes1705@users.noreply.github.com>
2024-01-21 21:53:36 +0300

    Solved another task
    Select colors from the list

---
+++ 07_python_study_case(name_colors_from_list).py
@@ -0,0 +1,11 @@
+# Study beginner tasks 
+# from https://www.w3resource.com/python-exercises/python-basic-exercises.php 
+
+# THE TASK 
+# Write a Python program to display the first and last colors from the following list.
+# color_list = ["Red","Green","White" ,"Black"]
+
+
+color_list = ["Red","Green","White" ,"Black"] 
+
+print("First and last color of the list are: " + str(color_list[0]) + " and " + str(color_list[-1]))
