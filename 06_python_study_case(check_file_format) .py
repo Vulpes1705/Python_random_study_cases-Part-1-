d4ac82813ec07cf8013438c87112702099e66620
Alexander <109859387+Vulpes1705@users.noreply.github.com>
2024-01-21 21:29:17 +0300

    Solved another task
    Write a Python program that accepts a filename from the user and prints the extension of the file.

---
+++ 06_python_study_case(check_file_format) .py
@@ -0,0 +1,18 @@
+# Study beginner tasks 
+# from https://www.w3resource.com/python-exercises/python-basic-exercises.php 
+
+# THE TASK 
+# Study beginner tasks 
+# from https://www.w3resource.com/python-exercises/python-basic-exercises.php 
+
+# THE TASK 
+# Write a Python program that accepts a filename from the user and prints the extension of the file.  
+
+
+# target_file = 
+# print(target_file)
+
+user_file = input("Please enter the full file name. e.g. \" my_file.txt\" ")
+result = user_file.split(".")[1]
+
+print(result)
