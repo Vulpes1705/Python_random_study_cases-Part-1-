2fffd18a2ebf3af5d4a0e8c0bac2044982ab8c64
Alexander <109859387+Vulpes1705@users.noreply.github.com>
2024-01-22 19:46:12 +0300

    Solved another task
    Multi-line string study

---
+++ 12_python_study_case(multilines string) .py
@@ -0,0 +1,18 @@
+# Study beginner tasks 
+# from https://www.w3resource.com/python-exercises/python-basic-exercises.php 
+
+# THE TASK 
+#   Write a Python program to print the following 'here document'.
+# Sample string :
+# a string that you "don't" have to escape
+# This
+# is a ....... multi-line
+# heredoc string --------> example
+
+
+print("""
+a string that you "don't" have to escape
+This
+is a ....... multi-line
+heredoc string --------> example
+""")
